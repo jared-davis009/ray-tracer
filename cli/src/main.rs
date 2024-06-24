92fa6b6f69c2c31751e4d753d3500f6ca21e7220
jared-davis009 <jared.davis009@gmail.com>
2024-06-24 15:49:14 -0400

    aspect ration, viewport, camera

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,5 @@
 use dotenv::dotenv;
-use ray_tracer::{Color, Vec3};
+use ray_tracer::{Color, Point3, Ray, Vec3};
 use std::{fs::File, sync::Arc};
 use tracing::{debug, trace, Level};
 
@@ -7,8 +7,49 @@ use tracing::{debug, trace, Level};
 
 fn main() {
     trace_setup();
-    let image_width = 256;
-    let image_height = 256;
+
+    // ------------Image Setup------------
+
+    let image_width = 400;
+    let aspect_ratio = 16.0 / 9.0; // Width / height
+
+    // Setting height based on width and aspect ratio
+    // Must be at least 1
+    let image_height = (image_width as f64 / aspect_ratio) as usize;
+    let image_height = if image_height > 1 { image_height } else { 1 };
+
+    // ------------Camera Setup------------
+
+    // Creating the viewport (rectangle in the 3d world which contains pixel grid)
+    let viewport_height = 2.0;
+    // We cannot use aspect_ratio for this due to it being the ideal value
+    // and not necessarily the real value
+    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
+
+    // Focal length is distance between camera center and viewport
+    let focal_length = 1.0;
+
+    // Point where all rays originate
+    let camera_center = Point3::new(0.0, 0.0, 0.0);
+
+    // Vector across x axis of viewport
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+
+    // Vector across y axis of viewport
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+
+    // Horizontal and delta vectors from pixel to pixel
+    let pixel_delta_u = viewport_u.div(image_width as f64);
+    let pixel_delta_v = viewport_v.div(image_height as f64);
+
+    let viewport_upper_left = camera_center.clone()
+        - Point3::new(0.0, 0.0, focal_length)
+        - viewport_u.div(2.0).into()
+        - viewport_v.div(2.0).into();
+    let pixel00_loc =
+        viewport_upper_left.add(0.5) * (pixel_delta_u.clone() + pixel_delta_v.clone()).into();
+
+    // ------------Render------------
 
     println!("P3"); // P3 means colors are ascii
     println!("{image_width} {image_height}");
@@ -16,10 +57,11 @@ fn main() {
     for j in 0..image_height {
         //trace!("Scanlines remaining {}", image_height - j);
         for i in 0..image_width {
-            let r: f64 = i as f64 / (image_width as f64 - 1.0);
-            let g: f64 = j as f64 / (image_height as f64 - 1.0);
-            let b = 0.0;
-            let pixel_color = Color::new(r, g, b);
+            let pixel_center = pixel00_loc.clone()
+                + ((pixel_delta_u.mul(i as f64)) + (pixel_delta_v.mul(j as f64))).into();
+            let ray_direction = pixel_center - camera_center.clone();
+            let ray = Ray::new(camera_center.clone(), ray_direction.into());
+            let pixel_color = ray.ray_color();
             pixel_color.write_color();
         }
     }
--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,4 +1,4 @@
-use std::ops::Add;
+use std::ops::{Add, Mul, Sub};
 
 #[derive(Debug, Default, Clone)]
 pub struct Vec3 {
@@ -31,6 +31,32 @@ impl Vec3 {
 
         Vec3 { x, y, z }
     }
+
+    pub fn div(&self, val: f64) -> Vec3 {
+        let x = self.x / val;
+        let y = self.y / val;
+        let z = self.z / val;
+
+        Vec3 { x, y, z }
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        self.div(self.length())
+    }
+
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+}
+
+impl From<Point3> for Vec3 {
+    fn from(point3: Point3) -> Self {
+        Vec3::new(point3.x, point3.y, point3.z)
+    }
 }
 
 impl Add for Vec3 {
@@ -47,18 +73,25 @@ impl Add for Vec3 {
 
 #[derive(Debug, Default)]
 pub struct Ray {
-    pub origin: Vec3,
+    pub origin: Point3,
     pub direction: Vec3,
 }
 
 impl Ray {
-    pub fn new(origin: Vec3, direction: Vec3) -> Self {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
         Ray { origin, direction }
     }
 
-    pub fn at(&self, t: f64) -> Vec3 {
+    pub fn at(&self, t: f64) -> Point3 {
         let multiplied = self.direction.mul(t);
-        self.origin.clone() + multiplied
+        self.origin.clone() + multiplied.into()
+    }
+
+    // See linear blend/interpolation
+    pub fn ray_color(&self) -> Color {
+        let unit_direction = self.direction.unit_vector();
+        let a = 0.5 * (unit_direction.y + 1.0);
+        Color::new(1.0, 1.0, 1.0).mul(1.0 - a) + Color::new(0.5, 0.7, 1.0).mul(a)
     }
 }
 
@@ -91,5 +124,108 @@ impl Color {
         let ig = (255.999 * self.g) as u32;
         let ib = (255.999 * self.b) as u32;
         println!("{ir} {ig} {ib}");
+    }
+
+    pub fn mul(&self, val: f64) -> Color {
+        let r = self.r * val;
+        let g = self.g * val;
+        let b = self.b * val;
+
+        Color { r, g, b }
+    }
+}
+
+impl Add for Color {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            r: self.r + other.r,
+            g: self.g + other.g,
+            b: self.b + other.b,
+        }
+    }
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct Point3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Point3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Point3 { x, y, z }
+    }
+
+    pub fn x(&mut self, val: f64) {
+        self.x = val;
+    }
+
+    pub fn y(&mut self, val: f64) {
+        self.y = val;
+    }
+
+    pub fn z(&mut self, val: f64) {
+        self.z = val;
+    }
+
+    pub fn mul(&self, val: f64) -> Point3 {
+        let x = self.x * val;
+        let y = self.y * val;
+        let z = self.z * val;
+
+        Point3 { x, y, z }
+    }
+
+    pub fn add(&self, val: f64) -> Point3 {
+        let x = self.x + val;
+        let y = self.y + val;
+        let z = self.z + val;
+
+        Point3 { x, y, z }
+    }
+}
+
+impl From<Vec3> for Point3 {
+    fn from(vec3: Vec3) -> Self {
+        Point3::new(vec3.x, vec3.y, vec3.z)
+    }
+}
+
+impl Add for Point3 {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Sub for Point3 {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul for Point3 {
+    type Output = Self;
+
+    fn mul(self, other: Self) -> Self {
+        Self {
+            x: self.x * other.x,
+            y: self.y * other.y,
+            z: self.z * other.z,
+        }
     }
 }
